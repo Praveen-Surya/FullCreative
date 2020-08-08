3c2189d53c6eed0304dad5e24d54f45ebdb04e07
PraveenSurya <praveen.surya@anywhere.co>
2020-08-08 15:16:01 +0530

    Created new classes

---
+++ JUnit/src/org/full/junit/ArithmeticOperations.java
@@ -0,0 +1,5 @@
+package org.full.junit;
+
+public class ArithmeticOperations {
+
+}
---
+++ JUnit/src/org/full/junit/TestArithmeticOperations.java
@@ -0,0 +1,14 @@
+package org.full.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestArithmeticOperations {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
